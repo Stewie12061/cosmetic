344b738138102ed008290fcad06740683609b284
Stewie12061 <nhut170501@gmail.com>
2021-10-28 15:13:07 +0700

    add lozad

---
+++ app/helpers/image_helper.rb
@@ -0,0 +1,15 @@
+module ImageHelper
+  def thumbnail(image)
+    image.nil? ? default_image : url_for(image)
+  end
+
+  def default_image
+    asset_pack_path 'media/images/mm.png'
+  end
+
+  def lozad_class(image)
+    %Q(
+      data-background-image=#{thumbnail(image)}
+    )
+  end
+end
